5e01220ab25c0b126ba66d9e00524e26e76b88c4
SaraKing <sking@vertafore.com>
2017-09-11 16:28:05 -0600

    new changes

--- TestCase/Program.cs
+++ TestCase/Program.cs
@@ -11,29 +11,12 @@ namespace TestCase
         static string keyName = "Document1.docx";
         static IAmazonS3 client;
  
-    
+     
 
         public static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Retrieving (GET) an object");
-                string data = ReadObjectData();
-            }
-            catch (AmazonS3Exception s3Exception)
-            {
-                Console.WriteLine(s3Exception.Message,
-                                  s3Exception.InnerException);
-            }
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-        }
 
-        static string ReadObjectData()
-        {
-            string responseBody = "";
-
-             using (client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2))
+            using (client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2))
             {
                 GetObjectRequest request = new GetObjectRequest
                 {
@@ -42,16 +25,13 @@ namespace TestCase
                 };
 
                 using (GetObjectResponse response = client.GetObject(request))
-                using (Stream responseStream = response.ResponseStream)
-                using (StreamReader reader = new StreamReader(responseStream))
                 {
-                    string title = response.Metadata["x-amz-meta-title"];
-                    Console.WriteLine("The object's title is {0}", title);
-
-                    responseBody = reader.ReadToEnd();
+                    Console.WriteLine(response);
+                   
                 }
             }
-            return responseBody;
+            Console.WriteLine(client);
+            Console.ReadLine();
         }
     }
 }
